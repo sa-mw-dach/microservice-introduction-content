e26736070dc4a54235ca7ec0c77cc906e84656af
Robert Bohne <robert.bohne@redhat.com>
2023-08-25 16:57:34 +0200

    Remove gif converting from screen records

--- record.sh
+++ record.sh
@@ -13,11 +13,12 @@ WINDOW=$(swaymsg -t get_tree | \
 wf-recorder -g "$WINDOW" -f $FILE
 
 
-ffmpeg \
-  -ss 00:00:00.000 \
-  -i $FILE \
-  -vf "fps=10,scale=1024:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
-  -loop 1 \
-  ${FILE}.gif
+#ffmpeg \
+#  -ss 00:00:00.000 \
+#  -i $FILE \
+#  -vf "fps=10,scale=1024:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
+#  -loop 1 \
+#  ${FILE}.gif
 
-echo "$FILE => ${FILE}.gif"
+echo "done: $FILE"
+
